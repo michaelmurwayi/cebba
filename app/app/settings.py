74604c4eb4b1108aa3a2933e500fcfd1fabaefe6
michael murwayi <mikemurwayi7@gmail.com>
2024-11-07 19:23:49 +0300

    adding website to django backend

--- app/app/settings.py
+++ app/app/settings.py
@@ -130,3 +130,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Google's SMTP server
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'michael@cebba.org'  # Your Google Workspace email address
+EMAIL_HOST_PASSWORD = 'Adambeck2019'  # App-specific password
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- app/web/views.py
+++ app/web/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomeView(TemplateView):
     template_name = "index.html"
+
+    def post(self, request):
+        return None
