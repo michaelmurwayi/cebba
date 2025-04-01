f31a16ae0d61de8214dc4c1bb05096c27acfff27
Michael Murwayi <mikemurwayi7@gmail.com>
2025-04-01 05:04:31 -0400

    ft-homepage: Adding intial State objects for the homepage

--- v2/src/state/initialState.js
+++ v2/src/state/initialState.js
@@ -5,11 +5,50 @@ const initialState = {
         "Services",
         "Contact",
     ],
+    companyInfo: {
+        name: "COFFEE ESTATE BOURGEOISIE BROKERS AGENCY",
+        location: "Coffee Plaza,Exchange lane-Off haile Selassie Avenue",
+        contact: "+254 7229 190 29",
+        website: "www.cebba.ke",
+        logo: "",
+        slogan: "Experience the Enablers of Elixir Energy"
+
+    },
     fileResources: [
         { name: "Farmer Agreement", link: "/files/file1.pdf" },
         { name: "Sample Stock Summary", link: "/files/file2.pdf" },
         { name: "Sample Sales Summary", link: "/files/file3.pdf" }
         ],
+    HomePage: {
+        header:{},
+        about: {
+            image1: "",
+            image2: "",
+            text : "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        },
+        statistics:{
+            bags: "",
+            farmers: "",
+            gradeVariant: "",
+        }   
+    },
+    services: {
+        agronomy: {
+            title: "Agronomy",
+            image: "",
+            description: ""
+        },
+        marketing: {
+            title: "Marketing",
+            image: "",
+            description: ""
+        },
+        qualityControl: {
+            title: "Quality Control",
+            image: "",
+            description: ""
+        },
+    }
 };
 
 
