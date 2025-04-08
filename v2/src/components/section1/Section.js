f29a30a791dc2c826d53a7a36aceb168e8765478
Michael Murwayi <mikemurwayi7@gmail.com>
2025-04-08 06:31:40 -0400

    ft-about: Adding cebba vision div to about page

--- v2/src/components/section1/Section.js
+++ v2/src/components/section1/Section.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Box, Grid } from "@mui/material";
+import initialState from "../../state/initialState";
 
 const Section = () => {
+  const { companyInfo } = initialState;
+
   return (
     <Box sx={{ flexGrow: 1, px: { xs: 2, md: 4 }, py: 4 }}>
       <Grid
@@ -10,12 +13,12 @@ const Section = () => {
         justifyContent="center"
         alignItems="center"
       >
-        {/* Red Box with Wrapped Text */}
+        {/* Left Box: Title */}
         <Grid item xs={12} md="auto">
           <Box
             sx={{
               width: { xs: "100%", sm: "100%", md: 300 },
-              height: { xs: "auto", sm: 180, md: 600 },
+              height: { xs: "auto", md: 600 },
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
@@ -23,36 +26,42 @@ const Section = () => {
               px: 2,
               py: 2,
               color: "black",
-              fontWeight: "",
-              fontSize: { xs: "1.2rem", md: "4.5rem" },
+              fontSize: { xs: "1.8rem", sm: "2.5rem", md: "4.5rem" },
               borderRadius: 2,
               wordWrap: "break-word",
               whiteSpace: "normal",
+              flexDirection: "column",
             }}
           >
-            CEBBA VISION
+            <span>
+              CEBBA{" "}
+              <span style={{ color: "rgb(205,127,50)" /* your color choice */ }}>
+                VISION
+              </span>
+            </span>
           </Box>
         </Grid>
 
-        {/* Blue Box */}
+        {/* Right Box: Vision Text */}
         <Grid item xs={12} md="auto">
           <Box
             sx={{
-              width: { xs: "100%", sm: "100%", md: 300 },
-              height: { xs: 150, sm: 180, md: 600 },
-              backgroundColor: "blue",
+              width: { xs: "100%", sm: "100%", md: 700 },
+              height: { xs: 300, sm: 400, md: 600 },
+              backgroundColor: "#D9D9D9",
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
-              color: "white",
-              fontWeight: "bold",
-              fontSize: { xs: "1.2rem", md: "2.5rem" },
-              borderRadius: 2,
+              color: "black",
+              fontSize: { xs: "1rem", sm: "1.2rem", md: "1.8rem" },
               textAlign: "center",
-              px: 2,
+              py: 2,
+              overflow: "auto",
+              overflowWrap: "break-word",
+              whiteSpace: "normal",
             }}
           >
-            Blue Box
+            {companyInfo.vision}
           </Box>
         </Grid>
       </Grid>
--- v2/src/features/about/About.js
+++ v2/src/features/about/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar  from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import about from "../../assets/about.jpg";
+import Section1 from "../../components/section1/Section";
 
 const About = () => {
     return (
@@ -11,6 +12,7 @@ const About = () => {
             title="ABOUT CEBBA"
             bgImage= {about}
             />
+            <Section1 />
         </div>
     );
 }
