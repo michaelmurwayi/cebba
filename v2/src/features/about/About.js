e652e228e644aada81a31f558f0e2ac4f53d8d50
Michael Murwayi <mikemurwayi7@gmail.com>
2025-04-08 05:24:20 -0400

    ft-about: Adding header title and background image

--- v2/src/features/about/About.js
+++ v2/src/features/about/About.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar  from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+import about from "../../assets/about.jpg";
 
 const About = () => {
     return (
         <div>
             <Navbar />
+            <Header 
+            title="ABOUT CEBBA"
+            bgImage= {about}
+            />
         </div>
     );
 }
