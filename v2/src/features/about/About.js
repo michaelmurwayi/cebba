cc0c5d3460944e1f4b9d02ad79ad3a088075b91c
Michael Murwayi <mikemurwayi7@gmail.com>
2025-04-08 07:11:29 -0400

    ft-about: removing overflow in banner container

--- v2/src/features/about/About.js
+++ v2/src/features/about/About.js
@@ -5,6 +5,7 @@ import about from "../../assets/about.jpg";
 import Section1 from "../../components/section1/Section";
 import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { styled } from "@mui/system";
+import beans from "../../assets/beans.png";
 
 const About = () => {
     // Use the theme hook for useMediaQuery
@@ -18,15 +19,16 @@ const About = () => {
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#000',
-        padding: theme.spacing(2),
+        overflow: 'hidden', // Prevents overflow
         [theme.breakpoints.down('sm')]: {
           flexDirection: 'column',
         },
     }));
       
     const CoffeeImage = styled("img")(({ theme }) => ({
-        height: 50,
-        width: 50,
+        height: 80,
+        width: 'auto', // Make width auto to maintain aspect ratio
+        maxWidth: '100%', // Ensure images don't exceed their container width
         objectFit: 'contain',
         margin: theme.spacing(1),
     }));
@@ -40,15 +42,15 @@ const About = () => {
             />
             <Section1 />
             <BannerContainer>
-                <CoffeeImage src="/mnt/data/band.png" alt="Coffee beans left" />
+                <CoffeeImage src={beans} alt="Coffee beans left" />
                 <Typography
-                    variant={isMobile ? "body1" : "h6"}
+                    variant={isMobile ? "body1" : "h4"}
                     color="white"
                     sx={{ textAlign: 'center', fontStyle: 'italic', marginX: 2 }}
                 >
                     Great brands start with great coffee
                 </Typography>
-                <CoffeeImage src="/mnt/data/band.png" alt="Coffee beans right" />
+                <CoffeeImage src={beans} alt="Coffee beans right" />
             </BannerContainer>
         </div>
     );
