c73d1d8c79b1591b2479408823904805e4afdb0b
Michael Murwayi <mikemurwayi7@gmail.com>
2025-04-09 08:15:16 -0400

    ft-service: Adding navbar component

---
+++ v2/src/features/services/Services.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
+
+const Services = () => {
+    return (
+        <div className="services">
+            <Navbar />
+        </div>
+    );
+}
+export default Services;
