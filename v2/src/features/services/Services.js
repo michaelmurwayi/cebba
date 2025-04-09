40430b4a90cb184bd3991839e33d9b09ed2d2da4
Michael Murwayi <mikemurwayi7@gmail.com>
2025-04-09 08:30:39 -0400

    ft-service: Adding headed component and header background image

--- v2/src/features/services/Services.js
+++ v2/src/features/services/Services.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import service from "../../assets/service.png";
 
 const Services = () => {
     return (
         <div className="services">
             <Navbar />
+            <Header 
+            title="Our Services"
+            bgImage={service}
+            />
         </div>
     );
 }
