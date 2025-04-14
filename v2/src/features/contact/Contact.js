c3cc55da0c351c91bd5153b9f6bbb9c6fb0cb002
michael murwayi <mikemurwayi7@gmail.com>
2025-04-14 12:41:30 -0500

    ft-contact-page: Adding maps to contact page

--- v2/src/features/contact/Contact.js
+++ v2/src/features/contact/Contact.js
@@ -1,15 +1,26 @@
 import React from "react"
 
 import Navbar from "../../components/navbar/Navbar"
+import Header from "../../components/header/Header"
+import Contact from "../../components/contact/Contact"
+import Footer from "../../components/footer/Footer"
+import Map from "../../components/map/Map"
+
+import contact from "../../assets/contact.jpg"
 
 
-const Contact = () => {
+
+const contactUs = () => {
     return (
         <div>
             <Navbar />
+            <Header title="Contact US" bgImage={contact} />
+            <Contact />
+            <Map/>
+            <Footer />
         </div>
     )
 }
 
 
-export default Contact
+export default contactUs
